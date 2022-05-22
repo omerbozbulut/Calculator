d9fe084ce446a53d0b33939d0a5670e03f4de8d3
omer <omerbzblt613@gmail.com>
2022-05-22 19:15:01 +0300

    init

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by omer faruk bozbulut on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
